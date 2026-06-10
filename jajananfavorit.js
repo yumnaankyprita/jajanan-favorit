36e56f0bb9a2bc822362cea1386b451258b67729
yumnaankyprita <yumnaankyprita@gmail.com>
2026-06-10 22:49:16 +0700

    jajananfavorit

---
+++ jajananfavorit.js
@@ -0,0 +1,12 @@
+const jajanan1 = "Piscok";
+const jajanan2 = "Churros";
+const jajanan3 = "Cilok";
+const jajanan4 = "Cimol";
+const jajanan5 = "Cireng";
+
+console.log("Daftar Jajanan Favorit Saya:");
+console.log("1. " + jajanan1);
+console.log("2. " + jajanan2);
+console.log("3. " + jajanan3);
+console.log("4. " + jajanan4);
+console.log("5. " + jajanan5);
